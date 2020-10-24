96cd900aa4b3ae138043b7ac455a4168e7bb6ff7
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-24 15:53:01 -0700

    Seeds successfully created

--- seeds/02_resources.js
+++ seeds/02_resources.js
@@ -1,13 +1,9 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+exports.seed = async function(knex) {
+      await knex('resources').insert([
+        {name: 'Cohort', description: 'Look in your help channel, lecture notes, and for help from TL'},
+        {name: 'W3shools', description: 'A resource for coding help'},
+        {name: 'Documentation', description: 'Read the documentation for a library or software'},
+        {name: 'stackOverflow', description: 'A help thread for code troubles'}
       ]);
-    });
 };
--- seeds/03_tasks.js
+++ seeds/03_tasks.js
@@ -1,13 +1,11 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+exports.seed = async function(knex) {
+  await knex('tasks').insert([
+        {description: 'Create a repository', project_id: 3},
+        {description: 'Practice adding, commiting, pushing, and merging', project_id: 3},
+        {description: 'Use knex to create a database', project_id: 1},
+        {description: 'Create endpoints for the database', project_id: 1},
+        {description: 'Rewrite a past project with flexbox', project_id: 2},
+        {description: 'Use media query breakpoints', project_id: 2},
       ]);
-    });
 };
